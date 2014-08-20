0088934361f96d10c0dbd2eb98091bff65f53cf3
s-tajima <tajima1989@gmail.com>
2014-08-20 23:53:07 +0900

    fix

--- site-cookbooks/basic-pack/recipes/default.rb
+++ site-cookbooks/basic-pack/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-%w{git screen}.each do |pkg|
+%w{git screen rubygems}.each do |pkg|
   package pkg do
     action :install
   end
--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
 file_cache_path "/tmp/chef-solo"
-cookbook_path ["/root/chef-repo/site-cookbooks"]
+cookbook_path ["/root/chef-repo/site-cookbooks", "/root/chef-repo/cookbooks"]
 ssl_verify_mode :verify_peer
 
