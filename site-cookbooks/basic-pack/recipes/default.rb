606e9819e40ad40eb36088b714835d48d11fa6ad
s-tajima <tajima1989@gmail.com>
2014-08-21 13:58:03 +0900

    fix

--- site-cookbooks/basic-pack/recipes/default.rb
+++ site-cookbooks/basic-pack/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-%w{git screen rubygems}.each do |pkg|
+%w{git screen rubygems tcpdump yum-utils}.each do |pkg|
   package pkg do
     action :install
   end
