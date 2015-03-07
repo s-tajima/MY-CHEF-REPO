d8cb427dafff326118014ae98bd3f68abcf11cbf
s-tajima <tajima1989@gmail.com>
2015-03-07 12:24:15 +0900

    fix

--- site-cookbooks/basic-pack/recipes/default.rb
+++ site-cookbooks/basic-pack/recipes/default.rb
@@ -5,8 +5,24 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-%w{git screen rubygems tcpdump yum-utils}.each do |pkg|
+%w{
+  git
+  screen
+  rubygems
+  tcpdump
+  yum-utils
+  docker
+  bind-utils
+  wget
+  telnet
+  mercurial
+  tcpflow
+}.each do |pkg|
   package pkg do
     action :install
   end
+end
+
+service "docker" do
+  action   [ :enable, :start ]
 end
